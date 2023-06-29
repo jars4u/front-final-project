cf25f0224701100e913a00c82d5b2021d00ac3b2
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-06-29 20:57:12 +0000

    boostrap register form

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,8 +3,79 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h4>REGISTRATE</h4>
+	//FORMULARIO DE REGISTRO
+	<div className="container">
+		<div className="row justify-content-center">
+			<div className="col-12 col-sm-9 col-md-7 col-lg-6 col-lx-5 login_container">
+				<h2 className="text-center">
+					<strong>Registrate</strong>
+				</h2>
+
+				<form>
+					<div className="form-group mt-3">
+						<label htmlFor="fullname">Nombre y Apellido:</label>
+						<input
+							type="text"
+							className="form-control"
+							id="fullname"
+							name="fullname"
+							placeholder="Ingresa tu nombre completo"
+						// onChange={handleChange}
+						// value={user.name}
+						></input>
+					</div>
+
+					<div className="form-group mt-3">
+						<label htmlFor="email">Email:</label>
+						<input
+							type="email"
+							className="form-control"
+							id="email"
+							name="email"
+							placeholder="Ingresa tu dirección de correo"
+						// onChange={handleChange}
+						// value={user.email}
+						></input>
+					</div>
+
+					<div className="form-group mt-3">
+						<label htmlFor="password">Password:</label>
+						<input
+							type="password"
+							className="form-control"
+							id="password"
+							name="password"
+							placeholder="Ingresa un password"
+						// onChange={handleChange}
+						// value={user.password}
+						></input>
+					</div>
+
+
+					<div className="form-group mt-3">
+						<label htmlFor="avatar">Avatar:</label>
+						<input
+							type="file"
+							className="form-control"
+							id="avatar"
+							name="avatar"
+							placeholder="Escoge una imagen para tu avatar"
+						// onChange={handleChange}
+						// value={user.avatar}
+						></input>
+					</div>
+
+					<select className="form-select mt-3" aria-label="Default select example">
+						<option selected>Escoge tu tipo de cuenta:</option>
+						<option value="1">Usuario</option>
+						<option value="2">Restaurant</option>
+					</select>
+
+				</form>
+			</div>
+		</div >
+
+		{/* <h4><strong>REGISTRATE</strong></h4>
 		<input
 			ClassName="form"
 			type="text"
@@ -34,11 +105,11 @@ export const Home = () => (
 			placeholder="Ingrese su Contraseña"
 		></input>
 		<p>
-			Estoy de acuerdo con <a href="#">Terminos y Condiciones</a>
+		Estoy de acuerdo con <a href="#">Terminos y Condiciones</a>
 		</p>
 		<input ClassName="button" type="submit" value="Registrar"></input>
 		<p>
-			<a href="#">¿Ya tienes Cuenta?</a>
-		</p>
-	</div>
+		<a href="#">¿Ya tienes Cuenta?</a>
+	</p> */}
+	</div >
 );
